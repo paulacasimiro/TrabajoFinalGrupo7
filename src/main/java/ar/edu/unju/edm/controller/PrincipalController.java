89e315c3e3028992c0d02431e7e9138f6ac95265
Usuario <Usuario@DESKTOP-G66J3R6>
2023-06-23 11:32:33 -0300

    SECURITY -Ingrid

--- src/main/java/ar/edu/unju/edm/controller/PrincipalController.java
+++ src/main/java/ar/edu/unju/edm/controller/PrincipalController.java
@@ -1,9 +1,12 @@
 package ar.edu.unju.edm.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.edm.model.Paciente;
+import ar.edu.unju.edm.service.PacienteService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +14,51 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class PrincipalController {
 
 	@Autowired
-	Paciente usuariopaciente;
+	Paciente usuario;
 	
-	@GetMapping({"/","/principal","/home","/login"})
-	public ModelAndView cargarPrincipal(){
-		//
+	@GetMapping("/login")
+	public ModelAndView cargarPrincipal(){ 
 		ModelAndView modelAndView = new ModelAndView("princi");
-		modelAndView.addObject("usuario",usuariopaciente);
+		modelAndView.addObject("usuario",usuario);
 		return modelAndView;
 	}
 	
+@GetMapping ("/vistaAdministrador")
 	
+	public ModelAndView mostrarvistaAdministrador () {
+		
+		ModelAndView name= new ModelAndView ("vistaAdministrador"); 
+		return name;
+		
+	}
+
+//vista para usuario cuando se Registre 
+
+ @Autowired 
+	PacienteService pacienteService; 
+	
+	@Autowired 
+	Paciente unPaciente; 
+	 
+	 @GetMapping("/paciente2")
+	public ModelAndView registrarPaciente2(){
+		ModelAndView modelAndView = new ModelAndView("formularioPaciente2");
+		modelAndView.addObject("paciente", unPaciente);
+		modelAndView.addObject("band", false);
+				 
+		return modelAndView;
+	}
+ //se muestra solo los datos de los usuarios registrados --vistadeRegistroUsuario.html--
+ @PostMapping("/guardarPaciente2")
+	public ModelAndView guardarPaciente2(@ModelAttribute("paciente") Paciente pacienteConDatos) {
+		ModelAndView lista= new ModelAndView ("vistadeUsuario"); 
+		pacienteService.registrarPaciente(pacienteConDatos);
+		lista.addObject("listadoPaciente2", pacienteService.listarTodoslosPacientes()); 
+		
+		return lista;
+	} 
+	
+ 
+	
+
 }
